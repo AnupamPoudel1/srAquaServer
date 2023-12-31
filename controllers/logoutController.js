a1a5bbbf3666b10d0fdb7e3010a17cf2a40879e5
Anupam <anupampoudel78@gmail.com>
2023-12-31 23:14:18 +0545

    logout page work

---
+++ controllers/logoutController.js
@@ -0,0 +1,33 @@
+const userDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const handleLogut = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204) //no content ie no cookie found to clear
+    const refreshToken = cookies.jwt;
+
+    // check for refreshToken in database
+    const foundUser = userDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        return res.sendStatus(204);
+    }
+
+    //delete refresh token from database
+    const otherUsers = userDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+    const currentUser = { ...foundUser, refreshToken: '' };
+    userDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+        path.join(__dirname, '..', 'model', 'users.json'),
+        JSON.stringify(userDB.users)
+    )
+
+    res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.sendStatus(204);
+}
+
+module.exports = { handleLogut };
